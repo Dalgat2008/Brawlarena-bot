496942adc2bc858c257b1376e4307f56353005cc
Dalgat2008 <7djhcghghw@privaterelay.appleid.com>
2025-12-30 18:22:41 +0300

    Delete match_checker.py

--- bot/services/match_checker.py
+++
@@ -1,6 +0,0 @@
-from bot.brawl_api import get_battlelog
-
-async def check_match(player_tags: list[str]):
-    logs = [await get_battlelog(t) for t in player_tags]
-    # упрощённо: берем последний матч
-    return logs[0]["items"][0]
