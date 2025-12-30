2f250e7791add67ccb40b899564746ed3cf191d3
Dalgat2008 <7djhcghghw@privaterelay.appleid.com>
2025-12-30 16:55:02 +0300

    Update brawl_api.py

--- bot/brawl_api.py
+++ bot/brawl_api.py
@@ -1,46 +1,20 @@
-import requests
-from datetime import datetime, timedelta
+import aiohttp
+from bot.config import BRAWL_API_TOKEN
 
-BRAWL_API_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImJlYTIzOGRmLTRhM2UtNDE1Yi05MWNlLWVjMWYxNGJjZDNlOCIsImlhdCI6MTc2NzA4Mjc2NSwic3ViIjoiZGV2ZWxvcGVyLzdjY2UyYTQwLTEwYjgtMDg4MS0yMDdjLTBkN2JmNDI0NDQ1MCIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTA5LjE5Ni4xMDEuMTQ0Il0sInR5cGUiOiJjbGllbnQifV19.2WDjcYLyWJ_Bg7MqqiHWQLXxkZotHiup6jqVursfjqI-EeOFcD9rcp1e8_SFzSLcx7q1wgNB-41qM4zCtHzJEw"
 BASE_URL = "https://api.brawlstars.com/v1"
 
+headers = {
+    "Authorization": f"Bearer {BRAWL_API_TOKEN}"
+}
 
-def get_battlelog(player_tag: str):
+async def get_player(player_tag: str):
     tag = player_tag.replace("#", "%23")
-    headers = {
-        "Authorization": f"Bearer {BRAWL_API_TOKEN}"
-    }
-    r = requests.get(
-        f"{BASE_URL}/players/{tag}/battlelog",
-        headers=headers,
-        timeout=10
-    )
-    r.raise_for_status()
-    return r.json()["items"]
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.get(f"{BASE_URL}/players/{tag}") as r:
+            return await r.json()
 
-
-def find_common_battle(players_tags: list[str], start_time: datetime):
-    logs = []
-    for tag in players_tags:
-        logs.append(get_battlelog(tag))
-
-    for battle in logs[0]:
-        battle_time = datetime.strptime(
-            battle["battleTime"], "%Y%m%dT%H%M%S.%fZ"
-        )
-
-        if battle_time < start_time - timedelta(minutes=10):
-            continue
-
-        ids = sorted([p["tag"] for p in battle["battle"]["players"]])
-
-        if all(
-            any(
-                sorted([p["tag"] for p in b["battle"]["players"]]) == ids
-                for b in log
-            )
-            for log in logs
-        ):
-            return battle
-
-    return None
+async def get_battlelog(player_tag: str):
+    tag = player_tag.replace("#", "%23")
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.get(f"{BASE_URL}/players/{tag}/battlelog") as r:
+            return await r.json()
