60ee4f67c74d7786e50245dab6d42a04885421b5
Dalgat2008 <7djhcghghw@privaterelay.appleid.com>
2025-12-30 09:16:15 +0300

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = int(os.getenv("DB_PORT", 5432))
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+if not BOT_TOKEN:
+    raise RuntimeError("❌ BOT_TOKEN не найден в .env")
