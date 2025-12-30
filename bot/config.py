b1759b3297995eed3da98491dc3d9be6dc3b7b64
Dalgat2008 <7djhcghghw@privaterelay.appleid.com>
2025-12-30 16:47:05 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,5 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-DB_PATH = "brawlarena.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+BRAWL_API_TOKEN = os.getenv("BRAWL_API_TOKEN")
+CRYPTOBOT_API_TOKEN = os.getenv("CRYPTOBOT_API_TOKEN")
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS").split(",")))
